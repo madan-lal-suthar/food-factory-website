b72b51a36ba9c84a332fa2e782a385309dca7d25
madan-lal-suthar <madanram8875@gmail.com>
2026-06-30 08:00:21 +0530

    feat: initialize food-factory project with Vite, React, and Tailwind CSS
    - Added package.json with dependencies and scripts for development, building, and linting.
    - Included initial images for the project in the public directory.
    - Created App component with basic styling using Tailwind CSS.
    - Set up TypeScript configuration files for app and node environments.
    - Configured Vite with React and Tailwind CSS plugins.

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+function App() {
+
+  return (
+    <>
+      <p className='text-3xl font-bold underline text-center bg-blue-200 rounded-lg'>
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
